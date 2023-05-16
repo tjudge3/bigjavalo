e6f4ec69ac03cf5cba387be4f033f0c458bfeb29
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-16 12:55:50 -0400

    Added methods Added comments

--- src/HelloPrinter.java
+++ src/HelloPrinter.java
@@ -1,6 +1,9 @@
-public class HelloPrinter {
-    public static void main (String[] args)
-    {
-        System.out.println("Hello World!");
+public class HelloPrinter { //This is the main class (the blue print)
+    public static void main (String[] args) //This is the main method
+    {//Below are the instructions inside the method
+        System.out.println("Hello World!"); //Print - Echo - exactly that
+        System.out.println(3 + 4); //Do the calculation and print the results
+        System.out.print(0); //Print the integer without line break
+        System.out.print(7); //Print the integer without line break
     }
 }
