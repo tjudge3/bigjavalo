d425a413eb8c945c9d27ef21bf4d6a031bb12cc7
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-16 13:59:28 -0400

    Added methods

--- src/HelloPrinter.java
+++ src/HelloPrinter.java
@@ -5,5 +5,7 @@ public class HelloPrinter { //This is the main class (the blue print)
         System.out.println(3 + 4); //Do the calculation and print the results
         System.out.print(0); //Print the integer without line break
         System.out.print(7); //Print the integer without line break
+        System.out.println("New");
+        System.out.print("York");
     }
 }
