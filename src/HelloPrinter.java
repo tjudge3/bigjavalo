cd4f3af3015cd352189a6a24c9afa0e581971523
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-17 10:00:28 -0400

    Added methods and comments

--- src/HelloPrinter.java
+++ src/HelloPrinter.java
@@ -5,7 +5,9 @@ public class HelloPrinter { //This is the main class (the blue print)
         System.out.println(3 + 4); //Do the calculation and print the results
         System.out.print(0); //Print the integer without line break
         System.out.print(7); //Print the integer without line break
-        System.out.println("New");
-        System.out.print("York");
+        System.out.println("This"); //Carriage return after
+        System.out.printf("'%15s' %n", "New"); //Printing with formatting, functionally same as print otherwise
+        System.out.print("York"); //No line breakage
+
     }
 }
