5400ea3efd1d239c375b7506f77ad0003a543294
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-19 18:06:37 -0400

    Add Methods and Calculations

--- src/nameCon.java
+++ src/nameCon.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class nameCon {
     public static void main(String[] args)
     {
@@ -8,5 +10,26 @@ public class nameCon {
         System.out.println();
         String fullName = fname + " " + lname;
         System.out.print(fullName);
+        System.out.println();
+
+        String jobTitle = "Agent";
+        int employeeId = 7;
+        String bond = jobTitle + employeeId;
+        System.out.println(bond);
+
+        int total = 12;
+        System.out.print("The total is ");
+        System.out.println(total);
+        System.out.println();
+
+        System.out.println("The total is " + total);
+        System.out.println();
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter your name: ");
+        String cfname = in.next();
+        String clname = in.next();
+        System.out.println(cfname);
+        System.out.println(clname);
     }
 }
