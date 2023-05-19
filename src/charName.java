3941b303c5409a65f62d8ac67abb1c94658c25cd
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-19 18:46:12 -0400

    Add Methods and Calculations

---
+++ src/charName.java
@@ -0,0 +1,21 @@
+public class charName {
+    public static void main(String[] args)
+    {
+        String name = "Harry";
+        char start = name.charAt(0);
+        char last = name.charAt(4);
+
+        System.out.println(start + " " + last);
+
+        String fullname = "HarryMorgan";
+        String sub = fullname.substring(0,6);
+        System.out.println(sub);
+
+        String greeting = "Hello, World!";
+        String sub1 = greeting.substring(0,5);
+        System.out.println(sub1);
+        String sub2 = greeting.substring(7,12);
+        System.out.println(sub2);
+
+    }
+}
