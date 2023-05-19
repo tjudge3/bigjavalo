767bc830957edceb6728e25dcf6954ae71425c8f
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-19 17:41:01 -0400

    Initial string committ

---
+++ src/nameCon.java
@@ -0,0 +1,9 @@
+public class nameCon {
+    public static void main(String[] args)
+    {
+        String fname = "Harry";
+        String lname = "Morgan";
+        String name = fname + lname;
+        System.out.print(name);
+    }
+}
