c90197c51dd9b79fa1ba5f80942d77997e609685
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-19 19:10:27 -0400

    GUI input and calculations

--- src/StrInput.java
+++ src/StrInput.java
@@ -5,6 +5,8 @@ public class StrInput {
     {
         String input = JOptionPane.showInputDialog("Enter Price:");
         double price = Double.parseDouble(input);
+        final double TAX_RATE = 8.5;
+        price = price * (1 + TAX_RATE / 100);
         JOptionPane.showMessageDialog(null, "Price: " + price);
     }
 }
