995b295fcdc85ee10b5de55976d66f6dbb58090c
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-19 03:52:55 -0400

    Added new examples and code

---
+++ src/FormatOut.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class FormatOut {
+    public static void main(String[] args)
+    {
+        double price = 1.215962441314554;
+        System.out.printf("%.2f", price); // Use printf for print formatting
+    }
+}
