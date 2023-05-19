06857ed4ec935e9f9c5146c7209d2b197a6bccd5
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-05-19 03:58:44 -0400

    Added new examples and code

--- src/FormatOut.java
+++ src/FormatOut.java
@@ -4,6 +4,11 @@ public class FormatOut {
     public static void main(String[] args)
     {
         double price = 1.215962441314554;
+        int quantity = 24;
+        double total = 17.29;
         System.out.printf("%.2f", price); // Use printf for print formatting
+        System.out.printf("%10.2f", price); // Format specifier
+        System.out.println("");
+        System.out.printf("Quantity: %d Total: %10.2f", quantity, total);
     }
 }
