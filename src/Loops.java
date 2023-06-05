e01e75afb162cbab3d0ec2b31e4246b44f5e1b30
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-06-05 02:42:51 -0400

    Loop Commit

---
+++ src/Loops.java
@@ -0,0 +1,21 @@
+public class Loops {
+    public static void main(String[] args)
+    {
+        int i = 0;
+        int sum = 0;
+//        while (sum < 10)
+//        {
+//            i++;
+//            sum = sum + i;
+//            System.out.println(sum);
+//        }
+//          Infinite Loop
+//        while (sum < 10)
+//        {
+//            i++;
+//            sum = sum - i;
+//            System.out.println(sum);
+//        }
+
+    }
+}
