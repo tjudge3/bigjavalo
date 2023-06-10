139ad8b968a0368bf629248ade6329f965b37362
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-06-10 19:36:43 -0400

    Loop Commit

---
+++ src/ForLoopExample1.java
@@ -0,0 +1,12 @@
+public class ForLoopExample1 {
+    public static void main(String[] args)
+    {
+        int sum = 0;
+        for (int counter = 5; counter <= 10; counter++)
+        {
+            sum = sum + counter;
+            System.out.println(sum);
+        }
+
+    }
+}
