1cb6d5e3a942148082b7d8f04fb60ef8935567ed
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-06-15 10:26:02 -0400

    Loop Commit

--- src/FizzBuzz3.java
+++ src/FizzBuzz3.java
@@ -1,8 +1,8 @@
 public class FizzBuzz3 {
     public static void main(String[] args)
     {
-        int n;
-        for(n=1; n<=100; n++)
+//        int n;
+        for(int n=1; n<=100; n++)
         {
             if (n%3==0 && n%5==0)
             {
