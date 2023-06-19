76baece0d79605ba63d3bd460d21f3aa89712a0d
tjudge <67015636+tjudge3@users.noreply.github.com>
2023-06-19 16:35:36 -0400

    Sentinel Initial Committ

---
+++ src/SentinelDemo.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class SentinelDemo {
+    public static void main(String[] args)
+    {
+        double sum = 0;
+        int count = 0;
+        double salary = 0;
+        System.out.print("Enter salaries, -1 to finish: ");
+        Scanner in = new Scanner(System.in);
+
+        // Process data until the sentinel is entered
+
+        while (salary != -1)
+        {
+            salary = in.nextDouble();
+            if (salary != -1)
+            {
+                sum = sum + salary;
+                count++;
+            }
+        }
+
+        // Compute and print the average
+
+        if (count > 0)
+        {
+            double average = sum / count;
+            System.out.println("Average Salary: " + average);
+        }
+        else
+        {
+            System.out.println("No Data");
+        }
+    }
+}
