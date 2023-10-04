c8216b83cde35e66e929fc534de19c43fad81d8d
tjudge3 <67015636+tjudge3@users.noreply.github.com>
2023-10-04 06:29:05 -0400

    firstVowel

---
+++ src/FirstVowel.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class FirstVowel {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String word = in.next();
+        boolean found = . . .;
+        String ch = . . .;
+        int position = . . .;
+        while(. . .)
+        {
+            ch = word.substring(. . ., . . . + 1).toLowerCase();
+            if ("aeiou".contains(ch))
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        if (. . .)
+    }
+}
